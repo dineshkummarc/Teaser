b9043e7dc444b5a4942b885f4c959efcc543c18b
nick <eiu165@gmail.com>
2010-05-25 00:59:47 -0500

    added first entity

---
+++ src/Teaser.Entities/Base/BaseItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teaser.Entities.Base
+{
+    [Serializable]
+    public abstract class BaseItem
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ src/Teaser.Entities/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Teaser.Entities.Base;
+
+namespace Teaser.Entities
+{
+    public class Game : BaseItem
+    {
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public DateTime Date { get; set; }
+        public int Line { get; set; }
+        public int WeekId { get; set; }
+        public string HomeScore { get; set; }
+        public string AwayScore { get; set; }
+    }
+}
