37e4cde44c7bdf2d899ad82b3a0de01884561f33
nick <eiu165@gmail.com>
2010-06-05 10:22:55 -0500

    added gallio test

---
+++ src/Junk/Gallio.SimpleLibrary.Test/FibonacciTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Gallio.SimpleLibrary;
+using MbUnit.Framework;
+using WatiN.Core;
+
+namespace Gallio.SimpleLibrary.Test
+{
+    public class FibonacciTest
+    {
+        [Test]
+        public void Fibonacci_of_number_greater_than_one()
+        {
+            int result = Fibonacci.Calculate(6);
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void SearchForWatiNOnGoogle()
+        {
+
+            using (var browser = new IE("http://www.google.com"))
+            {
+                browser.TextField(Find.ByName("q")).TypeText("WatiN");
+                browser.Button(Find.ByName("btnG")).Click();
+
+                Assert.IsTrue(browser.ContainsText("WatiN"));
+            }
+        }
+
+    }
+}
