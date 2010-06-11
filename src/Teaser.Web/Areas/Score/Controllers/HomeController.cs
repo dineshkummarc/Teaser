160c9caf14daf7d4a6bd743008a7aabbf600595a
unknown <Nick@.(none)>
2010-06-10 21:54:26 -0700

    added new area

---
+++ src/Teaser.Web/Areas/Score/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Teaser.Web.Areas.Score.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Score/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/Teaser.Web/Areas/Score/ScoreAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Teaser.Web.Areas.Score
+{
+    public class ScoreAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Score";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Score_default",
+                "Score/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
