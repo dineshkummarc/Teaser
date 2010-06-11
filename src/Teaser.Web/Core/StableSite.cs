2f6b97ac718adcb43a2eb17175f40312902dbd0f
unknown <Nick@.(none)>
2010-06-10 23:31:01 -0700

    site stable

--- src/Teaser.Web/Core/StableSite.cs
+++ src/Teaser.Web/Core/StableSite.cs
@@ -5,16 +5,24 @@ namespace Teaser.Web.Core
 	public static class StableSite
 	{
 		public static class Nav
-		{
-			public static readonly string Products = "products";
+        {
+            public static readonly string Products = "products";
+            public static readonly string Score = "Score";
 		}
 
 		public static class Screen
-		{
-			public static class Product
-			{
-				public static readonly string Index = "productIndex";
-			}
+        {
+            public static class Product
+            {
+                public static readonly string Index = "productIndex";
+            }
+            public static class Score
+            {
+                public static class Home
+                {
+                    public static readonly string Index = "ScoreHomeIndex";
+                }
+            }
 		}
 	}
 }
